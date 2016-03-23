d213a973e0b9ebd053154e178fe86ab00573f0b0
David Bonet <webbonet@gmail.com>
2016-03-23 20:34:49 +0100

    Add trait for identify game messages

--- src/client/messages.rs
+++ src/client/messages.rs
@@ -1,9 +1,21 @@
-#[derive(Serialize, Deserialize, Debug)]
+pub trait Message {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct LoginMessage {
 	pub token: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MessageType {
 	pub t: String,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PlayerMove {
+	pub x: i32,
+	pub y: i32,
+}
+
+impl Message for LoginMessage {}
+impl Message for MessageType {}
+impl Message for PlayerMove {}
